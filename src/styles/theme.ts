fe9384dd7db42da9233d87981b02f9e6450d8c84
DimitryAlves <dimitryalvessilva@gmail.com>
2023-02-14 11:15:15 -0300

    home-fix

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,8 +2,8 @@ import {extendTheme} from '@chakra-ui/react';
 
 const breakpoints = {
   sm: '320px',
-  md: '580px',
-  lg: '960px',
+  md: '576px',
+  lg: '992px',
   xl: '1200px',
   '2xl': '1536px',
 }
