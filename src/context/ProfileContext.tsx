c5a4a72d2008ebc19d1c9b52e03aecd00e4aa215
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-06-07 14:16:09 -0300

    ProfileContext

---
+++ src/context/ProfileContext.tsx
@@ -0,0 +1,70 @@
+import { IJWT, IUserCredentials, IUserProfile } from '../@types/global/profile.types';
+import React, { createContext, useEffect, useState } from 'react';
+import { useApi } from '../hooks/useApi';
+
+interface IProfileContext {
+   isAuthenticated: boolean, setIsAuthenticated: Function,
+   JWT: IJWT | null, setJWT: Function,
+   user: IUserProfile | null,
+   handleLogin: (email: string, password: string) => Promise<void>,
+   handleLogout: () => void,
+}
+
+const ProfileContext = createContext<IProfileContext>({} as IProfileContext);
+
+const ProfileContextProvider = ({ children }: { children: React.ReactNode }) => {
+   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+   const [JWT, setJWT] = useState<IJWT | null>(null);
+   const [user, setUser] = useState<IUserProfile | null>(null);
+
+   const { authenticate } = useApi();
+
+   async function handleLogin (email: string, password: string) {
+      try {
+         const userCredentials = (await authenticate(email, password));
+         const { accessToken, refreshToken, profile } = userCredentials;
+
+         localStorage.setItem('promed_session', JSON.stringify(userCredentials));
+
+         setJWT({ accessToken, refreshToken });
+         setUser(profile);
+         setIsAuthenticated(true);
+      }
+      catch (error) {
+         console.log('handleLogin:', error);
+         return alert('Credenciais de usuário incorretas.');
+      }
+   }
+
+   function handleLogout () {
+      localStorage.removeItem('promed_session');
+      setIsAuthenticated(false);
+      setUser(null);
+      setJWT(null);
+   }
+
+   useEffect(() => {
+      const storedCredentials = localStorage.getItem('promed_session') as string;
+      if (storedCredentials) {
+         const { accessToken, refreshToken, profile } = JSON.parse(storedCredentials) as IUserCredentials;
+         setJWT({ accessToken, refreshToken });
+         setUser(profile);
+         setIsAuthenticated(true);
+      }
+   }, []);
+
+   return (
+      <ProfileContext.Provider
+         value={{
+            isAuthenticated, setIsAuthenticated,
+            JWT, setJWT,
+            user,
+            handleLogin,
+            handleLogout
+         }}>
+         {children}
+      </ProfileContext.Provider>
+   );
+}
+
+export { ProfileContext, ProfileContextProvider };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import { ChakraBaseProvider } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app';
-import theme from './styles/theme';
-import './styles/global.css';
+import { ProfileContextProvider } from "./context/ProfileContext";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import theme from "./styles/theme";
+import "./styles/global.css";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <ChakraBaseProvider theme={theme}>
-          <App />
-        </ChakraBaseProvider>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ProfileContextProvider>
+      <ChakraBaseProvider theme={theme}>
+        <App />
+      </ChakraBaseProvider>
+    </ProfileContextProvider>
   </React.StrictMode>
-)
+);
